188743e55f4c4a712d9e4581e0fcc5abc459be61
gumbosan <gumbosan@gmail.com>
2016-09-09 01:30:50 +1000

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -69,7 +69,7 @@ public class Application extends Controller {
 
         responseStr = new GsonBuilder().disableHtmlEscaping().create().toJson(new Result(responseStr , new Result.Status(url, result.getContentType(), result.getStatus())));
         //return responseStr;
-        return StringEscapeUtils.unescapeHtml(responseStr);
+        return StringEscapeUtils.unescapeJavaScript(responseStr);
     }
 
     public static String sanitizeJsonpParam(String s) {
